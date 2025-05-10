6ec0ef7033267de2224a3bfea8b95635191b640f
Sahana Tavari <sahana.jagadeeshtavari@externos-uy.cencosud.com>
2025-05-10 21:53:14 +0530

    first commit

---
+++ src/main/java/com/practice/chatsystem/ChatsystemApplication.java
@@ -0,0 +1,13 @@
+package com.practice.chatsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatsystemApplication.class, args);
+	}
+
+}
